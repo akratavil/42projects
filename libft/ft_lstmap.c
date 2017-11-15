2c055f5ca872ede18e33f4d080b688dd34211ab8
Anthony KRATAVIL <akratavi@e3r12p8.42.fr>
2017-11-15 13:19:52 +0100

    Fillit and GNL in progress

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: akratavi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/08 18:50:36 by akratavi          #+#    #+#             */
-/*   Updated: 2017/11/13 12:48:13 by akratavi         ###   ########.fr       */
+/*   Updated: 2017/11/15 11:14:37 by akratavi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,7 @@ t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 	new = NULL;
 	if (lst)
 	{
-		new = ft_lstnew(lst->content, lst->content_size);
-		new = f(new);
+		new = f(lst);
 		new->next = ft_lstmap(lst->next, f);
 	}
 	return (new);
