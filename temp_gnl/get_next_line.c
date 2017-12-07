331ab884d0f8b451676cc80d9e065071b14bc745
Anthony KRATAVIL <akratavi@e3r10p6.42.fr>
2017-12-07 19:24:55 +0100

    GNL

--- temp_gnl/get_next_line.c
+++ temp_gnl/get_next_line.c
@@ -5,145 +5,118 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: akratavi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/12/05 16:14:24 by akratavi          #+#    #+#             */
-/*   Updated: 2017/12/06 20:08:24 by akratavi         ###   ########.fr       */
+/*   Created: 2017/12/07 17:52:19 by akratavi          #+#    #+#             */
+/*   Updated: 2017/12/07 19:22:52 by akratavi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-#include <stdlib.h>
 #include "libft/libft.h"
 #include "get_next_line.h"
 
-static int		copy_buff(char *buff, char **line)
+int		get_next_line(int const fd, char **line)
 {
+	static t_line	*next = NULL;
 	char			*tmp;
-	size_t			i;
-
-	i = 0;
-	while (i < BUFF_SIZE && buff[i] != '\n' && buff[i] != '\0')
-		i++;
-	if (!(tmp = ft_strnew(ft_strlen(*line) + i + 2)))
-		return (-1);
-	tmp = ft_strcpy(tmp, *line);
-	tmp = ft_strncat(tmp, buff, i);
-	free(*line);
-	*line = tmp;
-	if (i + 1 < BUFF_SIZE && buff[i] == '\n' && buff[i] != '\0')
-		return (1);
-	return (0);
-}
-
-static t_line	**save_next_line(int const fd, char *buff, t_line **next_line)
-{
-	size_t			i;
-	t_line			*new;
-	t_line			*lst_ptr;
-
-	i = 0;
-	if (!(new = (t_line*)malloc(sizeof(t_line))))
-		return (next_line);
-	while (buff[i] != '\n')
-		i++;
-	if (!(new->line = ft_strnew(BUFF_SIZE - i - 1)))
-	{
-		ft_memdel((void**)&new);
-		return (next_line);
-	}
-	new->line = ft_strncpy(new->line, &buff[i], BUFF_SIZE - i - 1);
-	new->fd = fd;
-	new->next = NULL;
-	if (!*next_line)
-	{
-		*next_line = new;
-		return (next_line);
-	}
-	lst_ptr = *next_line;
-	while (lst_ptr->next != NULL)
-		lst_ptr = lst_ptr->next;
-	lst_ptr->next = new;
-	return (next_line);
-}
-
-static int		check_next_line(int const fd, char **line, t_line **next_line)
-{
-	int				check;
-	t_line			*lst_ptr;
-	t_line			*lst_prv;
-
-	check = 0;
-	lst_ptr = *next_line;
-	lst_prv = NULL;
-	while (lst_ptr != NULL && lst_ptr->fd != fd)
-	{
-		ft_putendl("SEARCHING");
-		lst_prv = lst_ptr;
-		lst_ptr = lst_ptr->next;
-	}
-	if (lst_ptr)
-	{
-		check = copy_buff(lst_ptr->line, line);
-		ft_putendl("NEXT LINE COPIED");
-		if (lst_prv)
-			lst_prv->next = lst_ptr->next;
-		if (check == 1)
-		{
-			next_line = save_next_line(fd, lst_ptr->line, next_line);
-			ft_putendl("NEXT LINE SAVED");
-		}
-		free(lst_ptr->line);
-		free(lst_ptr);
-		ft_putendl("OLD NODE FREED");
-	}
-	return (check);
-}
-
-int				get_next_line(int const fd, char **line)
-{
 	char			buff[BUFF_SIZE];
+	size_t			i;
 	int				check;
-	int				i;
-	static t_line	**next_line = NULL;
 
 	if (fd < 0 || fd == 1 || fd == 2 || !line)
 		return (-1);
-	ft_putendl("FILE DESCRIPTOR CHECKED");
-	if (!(*line = ft_strnew(0)))
-		return (-1);
-	ft_putendl("VARIABLE \"line\" CLEARED");
-	if (!next_line)
-		if (!(next_line = (t_line**)ft_memalloc(sizeof(t_line*))))
+
+	ft_putendl("FD CHECKED");
+
+	if (*line)
+		ft_strdel(line);
+	if (!(*line = ft_strnew(BUFF_SIZE)))
 			return (-1);
-	ft_putendl("CHAINED LIST IS SET");
-	if (*next_line)
-		if ((check = check_next_line(fd, line, next_line)))
-			return (check);
-	ft_putendl("SAVED NEXT LINES CHECKED");
+
+	ft_putendl("LINE CREATED OR CLEARED");
+
+	if (!next)
+	{
+		if (!(next = (t_line*)ft_memalloc(sizeof(t_line))))
+			return (-1);
+		next->line = NULL;
+		next->fd = fd;
+
+		ft_putendl("STATIC SET");
+	}
+	else if (next->fd != fd && next->line)
+	{
+		ft_strdel(&next->line);
+		next->fd = fd;
+
+		ft_putendl("STATIC CLEARED");
+	}
+	else
+	{
+		tmp = next->line;
+		i = 0;
+		while (tmp[i] != '\n' && i < ft_strlen(tmp))
+			i++;
+		*line = ft_strncpy(*line, tmp, i);
+
+		ft_putendl("SAVED LINE RECOVERED");
+
+		if (i < ft_strlen(tmp) && tmp[i] == '\n')
+		{
+			if (!(next->line = ft_strnew(BUFF_SIZE)))
+				return (-1);
+			next->line = ft_strcpy(next->line, &tmp[i + 1]);
+			ft_strdel(&tmp);
+			return (1);
+		}
+		ft_strdel(&tmp);
+	}
+
 	while ((check = read(fd, buff, BUFF_SIZE)) && check != -1)
 	{
-		if (check == -1)
-			return (-1);
-		ft_putendl("BUFF CONTAINS: ");
 		i = 0;
-		while (i < BUFF_SIZE)
-		{
-			ft_putchar(buff[i]);
+		while (buff[i] != '\n' && (int)i < check)
 			i++;
-		}
-		ft_putchar('\n');
-		check = copy_buff(buff, line);
-		ft_putendl("LINE CONTAINS: ");
-		ft_putendl(*line);
-		if (check)
+		if (!(tmp = ft_strnew(ft_strlen(*line) + i + 2)))
+			return (-1);
+		tmp = ft_strcpy(tmp, *line);
+		tmp = ft_strncat(tmp, buff, i);
+		ft_strdel(line);
+		*line = tmp;
+
+		ft_putendl("BUFF COPIED");
+
+		if (i < BUFF_SIZE && buff[i] == '\n')
 		{
-			if (check == 1)
-			{
-				next_line = save_next_line(fd, buff, next_line);
-				ft_putendl("NEXT LINE SAVED");
-			}
-			return (check);
+			if (!(next->line = ft_strnew(BUFF_SIZE)))
+				return (-1);
+			i += 1;
+			next->line = ft_strncpy(next->line, &buff[i], BUFF_SIZE - i);
+
+			ft_putendl("NEXT LINE SAVED");
+
+			return (1);
 		}
 		ft_bzero(buff, BUFF_SIZE);
 	}
-	return (0);
+/*
+	ft_putendl("CLEARING BEFORE LEAVING");
+
+	if (*line)
+		ft_strdel(line);
+
+	ft_putendl("LINE CLEARED");
+
+	if (next)
+	{
+		if (next->line)
+			ft_strdel(&next->line);
+
+		ft_putendl("NEXT LINE DELETED");
+
+		ft_memdel((void**)&next);
+
+		ft_putendl("STATIC DELETED");
+	}
+*/
+	return (check);
 }
--- temp_gnl/get_next_line.h
+++ temp_gnl/get_next_line.h
@@ -6,19 +6,18 @@
 /*   By: akratavi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/15 11:27:49 by akratavi          #+#    #+#             */
-/*   Updated: 2017/12/06 17:39:09 by akratavi         ###   ########.fr       */
+/*   Updated: 2017/12/07 19:22:06 by akratavi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 32
+# define BUFF_SIZE 40
 
 typedef struct			s_line
 {
 	char				*line;
-	struct s_line		*next;
 	int					fd;
 }						t_line;
 
