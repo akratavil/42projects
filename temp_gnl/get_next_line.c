2792fc18d2cd3c999d45a7920fdda6d926b8fb2c
Anthony KRATAVIL <akratavi@e3r2p8.42.fr>
2017-12-08 18:56:52 +0100

    GNL 0.8

--- temp_gnl/get_next_line.c
+++ temp_gnl/get_next_line.c
@@ -6,117 +6,70 @@
 /*   By: akratavi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/07 17:52:19 by akratavi          #+#    #+#             */
-/*   Updated: 2017/12/07 19:22:52 by akratavi         ###   ########.fr       */
+/*   Updated: 2017/12/08 18:55:13 by akratavi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdlib.h>
 #include "libft/libft.h"
 #include "get_next_line.h"
+
+static char		**read_file(int const fd)
+{
+	char			**tab;
+	char			*tmp;
+	char			*dst;
+	char			buff[BUFF_SIZE];
+	int				check;
+
+	tmp = NULL;
+	dst = ft_strnew(0);
+	while ((check = read(fd, buff, BUFF_SIZE)) && check != -1)
+	{
+		if (!(tmp = ft_strnew(ft_strlen(dst) + check + 1)))
+			return (NULL);
+		tmp = ft_strcpy(tmp, dst);
+		tmp = ft_strncat(tmp, buff, check);
+		if (dst)
+			free(dst);
+		dst = tmp;
+	}
+	tab = ft_strsplit(dst, '\n');
+	free(dst);
+	dst = NULL;
+	return (tab);
+}
 
 int		get_next_line(int const fd, char **line)
 {
 	static t_line	*next = NULL;
 	char			*tmp;
-	char			buff[BUFF_SIZE];
-	size_t			i;
-	int				check;
 
 	if (fd < 0 || fd == 1 || fd == 2 || !line)
 		return (-1);
-
-	ft_putendl("FD CHECKED");
-
-	if (*line)
-		ft_strdel(line);
-	if (!(*line = ft_strnew(BUFF_SIZE)))
-			return (-1);
-
-	ft_putendl("LINE CREATED OR CLEARED");
-
 	if (!next)
 	{
 		if (!(next = (t_line*)ft_memalloc(sizeof(t_line))))
 			return (-1);
-		next->line = NULL;
+		next->lines = read_file(fd);
 		next->fd = fd;
-
-		ft_putendl("STATIC SET");
 	}
-	else if (next->fd != fd && next->line)
+	else if (next->fd != fd && next->lines)
 	{
-		ft_strdel(&next->line);
-		next->fd = fd;
-
-		ft_putendl("STATIC CLEARED");
-	}
-	else
-	{
-		tmp = next->line;
-		i = 0;
-		while (tmp[i] != '\n' && i < ft_strlen(tmp))
-			i++;
-		*line = ft_strncpy(*line, tmp, i);
-
-		ft_putendl("SAVED LINE RECOVERED");
-
-		if (i < ft_strlen(tmp) && tmp[i] == '\n')
+		while (*(next->lines))
 		{
-			if (!(next->line = ft_strnew(BUFF_SIZE)))
-				return (-1);
-			next->line = ft_strcpy(next->line, &tmp[i + 1]);
+			tmp = *(next->lines);
+			next->lines += 1;
 			ft_strdel(&tmp);
-			return (1);
 		}
-		ft_strdel(&tmp);
+		next->lines = read_file(fd);
+		next->fd = fd;
 	}
-
-	while ((check = read(fd, buff, BUFF_SIZE)) && check != -1)
-	{
-		i = 0;
-		while (buff[i] != '\n' && (int)i < check)
-			i++;
-		if (!(tmp = ft_strnew(ft_strlen(*line) + i + 2)))
-			return (-1);
-		tmp = ft_strcpy(tmp, *line);
-		tmp = ft_strncat(tmp, buff, i);
-		ft_strdel(line);
-		*line = tmp;
-
-		ft_putendl("BUFF COPIED");
-
-		if (i < BUFF_SIZE && buff[i] == '\n')
-		{
-			if (!(next->line = ft_strnew(BUFF_SIZE)))
-				return (-1);
-			i += 1;
-			next->line = ft_strncpy(next->line, &buff[i], BUFF_SIZE - i);
-
-			ft_putendl("NEXT LINE SAVED");
-
-			return (1);
-		}
-		ft_bzero(buff, BUFF_SIZE);
-	}
-/*
-	ft_putendl("CLEARING BEFORE LEAVING");
-
+	*line = *(next->lines);
+	next->lines += 1;
 	if (*line)
-		ft_strdel(line);
-
-	ft_putendl("LINE CLEARED");
-
-	if (next)
-	{
-		if (next->line)
-			ft_strdel(&next->line);
-
-		ft_putendl("NEXT LINE DELETED");
-
-		ft_memdel((void**)&next);
-
-		ft_putendl("STATIC DELETED");
-	}
-*/
-	return (check);
+		return (1);
+	else
+		return (0);
 }
